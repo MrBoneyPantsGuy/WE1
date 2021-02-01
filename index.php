ab59f00baa488673fbed76b6b9dc970c644ea64a
MrBoneyPantsGuy <64370314+MrBoneyPantsGuy@users.noreply.github.com>
2021-02-01 18:43:33 +0100

    Transition to php and javscript navigation via json structure

---
+++ index.php
@@ -0,0 +1,161 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WE - Landingpage</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <style>
+        body {
+            margin: 0;
+            font-family: 'Raleway';
+        }
+
+        #main-menu ul li {
+            display: inline;
+        }
+
+        #container {
+            display: grid;
+            width: 100%;
+            height: 100vh;
+            grid-template-columns: auto 1fr 1fr;
+            grid-template-rows: auto 1fr auto;
+            grid-template-areas:
+            "header header header"
+            "left content content"
+            "footer footer footer";
+        }
+
+        #main-menu {
+            grid-area: header;
+            background: dimgrey;
+        }
+
+        #sub-menu {
+            grid-area: left;
+            background: darkgrey;
+            display: none;
+            padding-left: 1em;
+            padding-right: 1em;
+        }
+
+        #content-area {
+            display: grid;
+            grid-area: content;
+            background: orange;
+            width: 100%;
+            grid-template-columns: 100%;
+            grid-template-rows: auto 1fr;
+            grid-template-areas:
+            "info"
+            "file";
+            overflow-y: scroll;
+        }
+
+        #info {
+            grid-area: info;
+            background: #4a77d4;
+            padding-left: 1em;
+            display: inline;
+        }
+
+        #codecontainer {
+            grid-area: file;
+            background: dimgrey;
+            padding: 1em;
+        }
+
+        .renderbutton {
+            margin-left: 1em;
+            padding: 0.5em;
+        }
+
+        .renderbutton a {
+            text-decoration: none;
+        }
+
+        .renderbutton a:visited {
+
+        }
+
+        renderbutton a:hover {
+
+        }
+
+        pre {
+            white-space: pre-wrap;
+            tab-size: 3;
+            margin-top: 0;
+            margin-bottom: 0;
+            color: gold;
+        }
+
+        #footer {
+            grid-area: footer;
+            background: dodgerblue;
+        }
+
+        ul.menu {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            background-color: #333;
+        }
+
+        ul.menu li {
+            float: left;
+        }
+
+        ul.menu li a {
+            display: inline-block;
+            color: white;
+            text-align: center;
+            padding: 1em 1em;
+            text-decoration: none;
+        }
+
+        ul.menu li a:hover:not(.active) {
+            background-color: #000;
+        }
+
+        ul.menu li a.active {
+            background-color: #1E90FF;
+        }
+    </style>
+</head>
+<body>
+    <div id="container">
+        <nav id="main-menu" >
+            <ul class="menu">
+                <li><a class="navigateMain">Woche 1</a></li>
+                <li><a class="navigateMain">Woche 2</a></li>
+                <li><a class="navigateMain">Woche 3</a></li>
+                <li><a class="navigateMain">Woche 4</a></li>
+                <li><a class="navigateMain">Woche 5</a></li>
+                <li><a class="navigateMain">Woche 6</a></li>
+                <li><a class="navigateMain">Woche 7</a></li>
+                <li><a class="navigateMain">Woche 8</a></li>
+                <li><a class="navigateMain">Woche 9</a></li>
+                <li><a class="navigateMain">Woche 10</a></li>
+            </ul>
+        </nav>
+        <nav id="sub-menu">
+            <h3 id="week"></h3>
+            <hr>
+            <ul id="files">
+            </ul>
+        </nav>
+        <div id="content-area">
+            <div id="info"><p>Beschreibung der Aufgabe</p></div>
+            <div id="codecontainer"><pre><code class="lang-html" id="code"></code></pre></div>
+        </div>
+        <footer id="footer">
+            <ul class="menu copyright">
+                <li><a href="https://www.h-brs.de/de/impressum" target="_blank">Impressum</a></li>
+            </ul>
+        </footer>
+    </div>
+    <script src="./src/functions.js"></script>
+</body>
+</html>
---
+++ src/functions.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const d = document;
+const getJsonData = async function() {
+    return await fetch("./src/jsonData.json").then(res => res.json());
+}
+const jsonData = getJsonData();
+
+// listen to all clicks on the main menu
+const main = document.querySelectorAll(".navigateMain");
+main.forEach(item => item.addEventListener('click', async function() {
+    const data = await jsonData;
+    const week = item.innerHTML;
+
+    // submenu
+    const subtitle = d.getElementById("week");
+    const subs = d.getElementById("files");
+    subs.innerHTML = ""; // clear list
+
+    // content area
+    const desc = d.getElementById("info");
+    const code = d.getElementById("code");
+
+    // appends all files to the list
+    subtitle.innerHTML = week;
+    d.getElementById("sub-menu").style.display = "block";
+
+    let id = 0;
+    data[week].files.forEach(elem => {
+        const entry = d.createElement("li");
+        entry.className = "navigateSub";
+        entry.setAttribute("id", (id++).toString());
+        const name = d.createTextNode(elem.name);
+        entry.appendChild(name);
+        subs.appendChild(entry);
+    });
+
+    // set the description and remove prior src code
+    desc.innerHTML = "<p>" + data[week].desc + "</p>";
+    code.innerText = ""
+
+    const subEntries = d.querySelectorAll(".navigateSub")
+        .forEach(sub => sub.addEventListener('click', async function() {
+            const filename = sub.innerHTML
+            const filepath = data[week].files[sub.id].pfad;
+            code.innerText = await fetch(filepath).then(res => res.text());
+            desc.innerHTML = "<p>" + data[week].files[sub.id].desc + "<button class='renderbutton'><a href='"+ data[week].files[sub.id].pfad +"' target='_blank'>In Aktion anzeigen!</a></button></p>";
+        }));
+}));
